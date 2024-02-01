7737a34cdecb6ce61409c7b3b33ee3ff40b486f1
Sandeep-508 <elsenothing321@gmail.com>
2024-02-01 12:10:06 +0530

    changed somes margins and add some chnages in footer and added color to the text of the scrollbar or navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,12 @@
 window.addEventListener('scroll', function () {
     let navbar = document.getElementById('navbar');
     let links = document.querySelectorAll(".item");
+    let span = this.document.querySelectorAll(".bar");
 
     if (window.scrollY > 500) {
         navbar.classList.add('scrolled');
         links.forEach(e => e.style.color = "white")
+        span.forEach(e => e.style.backgroundColor = "white")
     } else {
         navbar.classList.remove('scrolled');
         links.forEach(e => e.style.color = "black")
